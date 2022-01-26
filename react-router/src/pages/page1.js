4283adeec265ca41f8b1191289aad4b0fc20adcb
alfonsofdelarosa <alfonsofdelarosa@gmail.com>
2022-01-26 19:21:05 +0100

    cambios

--- react-router/src/pages/page1.js
+++ react-router/src/pages/page1.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
 
 const Page1 = () => {
   const navigate = useNavigate();
   return (
     <>
       <h1 style={{ marginTop: "50px" }}>PAGE 1</h1>
+      
       <button onClick={() => navigate(-1)}>Back</button>
       <button onClick={() => navigate("/home")}>Home</button>
     </>
   );
 };
-export default Page1;
+export default Page1 ;
+
+
